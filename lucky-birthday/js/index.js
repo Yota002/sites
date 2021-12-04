4c5202fa10a73bc88f7ad309f7a13d1f6ed4720d
Yota002 <moscowrus2013@gmail.com>
2021-12-04 23:54:07 +0500

    lucky birthday

---
+++ lucky-birthday/js/index.js
@@ -0,0 +1,27 @@
+const inputDay = document.getElementById("inputDay");
+const inputMonth = document.getElementById("inputMonth");
+const button = document.getElementById("button");
+const result = document.getElementById("result");
+
+fetch("../json/birthdays.json")
+    .then((response) => response.json())
+    .then((data) => {
+        let birthdays = data.days;
+        let daysInMonths = data.daysInMonths;
+        button.addEventListener("click", () => {
+            if (inputDay.value > daysInMonths[inputMonth.value]) {
+                result.textContent = `Error (${inputMonth.value} doesn't have ${inputDay.value} days)`;
+                return;
+            }
+
+            if (inputDay.value <= 0) {
+                result.textContent = `Error (${inputDay.value} day doesn't exist)`;
+                return;
+            }
+
+            birthdays.find(function (day, index) {
+                if (day.month == inputMonth.value && day.day == inputDay.value)
+                    result.textContent = `Your position in rating is ${index + 1}!`;
+            });
+        });
+    });
